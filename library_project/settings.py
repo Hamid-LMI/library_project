dcf64ae5f84666946b66010eef01fdaa60aa26d4
Hamid LMI <hamid.leulmi@laposte.net>
2025-08-23 01:59:42 +0200

    feat: configure Django logging to capture errors on Render

--- library_project/settings.py
+++ library_project/settings.py
@@ -173,3 +173,17 @@ REST_FRAMEWORK = {
 }
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
