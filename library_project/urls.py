b3ed714d6f283cf6ae5ec92db7525332002d96bf
Hamid LMI <hamid.leulmi@laposte.net>
2025-08-22 03:19:43 +0200

    feat(docker): add docker setup with backend, frontend and MySQL services

--- library_project/urls.py
+++ library_project/urls.py
@@ -3,13 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# urlpatterns = [
-#     path("library/", include("library.urls")),
-#     path('admin/', admin.site.urls)
-# ]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('books.urls')),
     path('', include('books.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
